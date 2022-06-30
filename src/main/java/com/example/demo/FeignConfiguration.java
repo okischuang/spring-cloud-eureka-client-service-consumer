11bd0a372866edaccbe8e078d14dc70a38aa0a54
oldcheese <okistheengineer@gmail.com>
2022-06-30 11:11:42 +0800

    update docker image, add config implementation

--- src/main/java/com/example/demo/FeignConfiguration.java
+++ src/main/java/com/example/demo/FeignConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -12,10 +14,14 @@ import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class FeignConfiguration {
+	
+	@Value("${request.timeout:3000}")	
+	private int timeout;
+	
 	@Bean
 	public Customizer<Resilience4JCircuitBreakerFactory> circuitBreakerFactoryCustomizer() {
-		System.out.println("config customize");
-		TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(10)).build();
+		System.out.printf("timeout config: %d msec", timeout);
+		TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(timeout)).build();
 		return resilience4JCircuitBreakerFactory -> resilience4JCircuitBreakerFactory
 				.configure(builder -> builder.timeLimiterConfig(timeLimiterConfig), "HelloClient#hello(int)");
 	}
--- src/main/java/com/example/demo/SpringCloudEurekaClientSeriveConsumerApplication.java
+++ src/main/java/com/example/demo/SpringCloudEurekaClientSeriveConsumerApplication.java
@@ -1,29 +1,18 @@
 package com.example.demo;
 
-import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @SpringBootApplication
@@ -48,14 +37,8 @@ public class SpringCloudEurekaClientSeriveConsumerApplication {
         return CompletableFuture.completedFuture("Fallback");
     }
 
-	public static void main(String[] args) {
-		TimeLimiterConfig config = TimeLimiterConfig.custom()
-				   .cancelRunningFuture(true)
-				   .timeoutDuration(Duration.ofMillis(10000))
-				   .build();
-		// Create a TimeLimiterRegistry with a custom global configuration
-		TimeLimiterRegistry timeLimiterRegistry = TimeLimiterRegistry.of(config);
-		
+	
+	public static void main(String[] args) {	
 		SpringApplication.run(SpringCloudEurekaClientSeriveConsumerApplication.class, args);
 	}
 
